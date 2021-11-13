f82cc54ed41d5056c2169373bc47ab57176194a4
Tyler Tucker <tyler@mutual.app>
2021-11-12 18:45:06 -0700

    link updates

--- app/src/main/java/com/bri1/soundbored2/Credits.kt
+++ app/src/main/java/com/bri1/soundbored2/Credits.kt
@@ -14,19 +14,28 @@ class Credits : AppCompatActivity() {
     }
 
     private fun setupHyperlink() {
+        // license
         val licenseLink = findViewById<TextView>(R.id.text_license_link)
         licenseLink.setMovementMethod(LinkMovementMethod.getInstance())
 
+        // andrej github
         val andrejLink = findViewById<TextView>(R.id.text_andrej)
         andrejLink.setMovementMethod(LinkMovementMethod.getInstance())
 
+        // webcooltz github
         val webcooltzLink = findViewById<TextView>(R.id.text_webcooltz)
         webcooltzLink.setMovementMethod(LinkMovementMethod.getInstance())
 
+        // open source
         val openSourceLink = findViewById<TextView>(R.id.text_open_source)
         openSourceLink.setMovementMethod(LinkMovementMethod.getInstance())
 
+        // flaticon
         val flatIconLink = findViewById<TextView>(R.id.text_icons)
         flatIconLink.setMovementMethod(LinkMovementMethod.getInstance())
+
+        // privacy policy
+        val privacyLink = findViewById<TextView>(R.id.privacy_policy)
+        privacyLink.setMovementMethod(LinkMovementMethod.getInstance())
     }
 }
